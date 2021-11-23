cb2694b19add83e0bc0f3e015e9e408f65db423f
Danis Omeragić <83123979+DanisOmeragic@users.noreply.github.com>
2021-11-23 21:16:36 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,102 @@
+radio.set_group(1)
+radio.send_string("yeah")
+
+charPos = 2
+turn = False
+gameOver = True
+started = False
+
+
+#PairSend
+def on_gesture_shake():
+    global gameOver, started, turn
+    if gameOver == True and started == False:
+        pairId = randint(11, 100)
+        print(pairId)
+        radio.send_number(pairId)
+        radio.set_group(pairId)  
+        gameOver = False
+        started = True
+        led.plot(charPos, 4)
+        turn = True
+
+
+#ReceiveStr
+def on_received_string(receivedString):
+    global gameOver
+    if receivedString == "Hit":
+        gameOver = True
+        basic.show_string("You Win")
+
+#ReceiveNum
+def on_received_number(receivedNumber):
+    global turn, charPos, gameOver, started
+    
+    #ReceiveBullet
+    if receivedNumber < 10:
+        turn = False
+        for i, in range(0, 6):
+            if i != 5:
+                led.plot(receivedNumber, i)
+                led.unplot(receivedNumber, i - 1)
+                pause(100)
+            else:
+                led.unplot(receivedNumber, i - 1)
+                if receivedNumber == charPos:
+                    gameOver = True
+                    radio.send_string("Hit")
+                    basic.show_string("You Lose")
+    
+        turn = True
+    #ReceivePair
+    else:
+        if gameOver == True and started == False:
+            radio.set_group(receivedNumber)
+            print("paired")
+            gameOver = False
+            started = True
+            led.plot(charPos, 4)
+
+
+#Move Left
+def on_button_pressed_a():
+    global charPos, turn
+    if turn == True and gameOver == False:
+        if charPos > 0:
+            led.unplot(charPos, 4)
+            charPos = charPos - 1
+            led.plot(charPos, 4)
+
+#Move Right
+def on_button_pressed_b():
+    global charPos, turn
+    if turn == True and gameOver == False:
+        if charPos < 4:
+            led.unplot(charPos, 4)
+            charPos = charPos + 1
+            led.plot(charPos, 4)
+
+#Fire
+def on_button_pressed_ab():
+    global charPos, turn
+
+    if turn == True and gameOver == False:
+        
+        for i, in range(0, 5):
+            led.plot(charPos, 3 - i)
+            if i != 0:
+                led.unplot(charPos, 4 - i)
+            print(3 - i)
+            pause(100)
+        radio.send_number(charPos)
+
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+radio.on_received_number(on_received_number)
+
+
+radio.on_received_string(on_received_string)
---
+++ main.ts
@@ -0,0 +1,106 @@
+radio.setGroup(1)
+radio.sendString("yeah")
+let charPos = 2
+let turn = false
+let gameOver = true
+let started = false
+// PairSend
+// ReceiveStr
+// ReceiveNum
+// Move Left
+// Move Right
+// Fire
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (turn == true && gameOver == false) {
+        if (charPos > 0) {
+            led.unplot(charPos, 4)
+            charPos = charPos - 1
+            led.plot(charPos, 4)
+        }
+        
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (turn == true && gameOver == false) {
+        if (charPos < 4) {
+            led.unplot(charPos, 4)
+            charPos = charPos + 1
+            led.plot(charPos, 4)
+        }
+        
+    }
+    
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
+    if (turn == true && gameOver == false) {
+        for (let i = 0; i < 5; i++) {
+            led.plot(charPos, 3 - i)
+            if (i != 0) {
+                led.unplot(charPos, 4 - i)
+            }
+            
+            console.log(3 - i)
+            pause(100)
+        }
+        radio.sendNumber(charPos)
+    }
+    
+})
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    let pairId: number;
+    
+    if (gameOver == true && started == false) {
+        pairId = randint(11, 100)
+        console.log(pairId)
+        radio.sendNumber(pairId)
+        radio.setGroup(pairId)
+        gameOver = false
+        started = true
+        led.plot(charPos, 4)
+        turn = true
+    }
+    
+})
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    
+    // ReceiveBullet
+    if (receivedNumber < 10) {
+        turn = false
+        for (let i = 0; i < 6; i++) {
+            if (i != 5) {
+                led.plot(receivedNumber, i)
+                led.unplot(receivedNumber, i - 1)
+                pause(100)
+            } else {
+                led.unplot(receivedNumber, i - 1)
+                if (receivedNumber == charPos) {
+                    gameOver = true
+                    radio.sendString("Hit")
+                    basic.showString("You Lose")
+                }
+                
+            }
+            
+        }
+        turn = true
+    } else if (gameOver == true && started == false) {
+        radio.setGroup(receivedNumber)
+        console.log("paired")
+        gameOver = false
+        started = true
+        led.plot(charPos, 4)
+    }
+    
+})
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    
+    if (receivedString == "Hit") {
+        gameOver = true
+        basic.showString("You Win")
+    }
+    
+})
